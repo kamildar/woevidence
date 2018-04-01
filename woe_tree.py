f5b1c65ee4e74a0cd0eb670223a2c8ddee812155
Ildar Kamaldinov <kamildraf@gmail.com>
2018-04-01 16:38:44 +0300

    add paralleling fit and transform

--- woe_tree.py
+++ woe_tree.py
@@ -2,6 +2,7 @@
 # MIT License
 from one_feature_tree import OneFeatureTree
 import numpy as np
+from joblib import Parallel, delayed
 
 
 class WoeTree(object):
@@ -12,12 +13,14 @@ class WoeTree(object):
                  min_samples_leaf=2,
                  min_samples_class=1,
                  smooth_woe=0.001,
+                 n_jobs=1,
                  dtype=np.float32):
         self._criterion = criterion
         self._max_depth = max_depth
         self._min_samples_leaf = min_samples_leaf
         self._min_samples_class = min_samples_class
         self._smooth_woe = smooth_woe
+        self._n_jobs = n_jobs
         self._dtype = dtype
 
         self._trees = []
@@ -27,6 +30,9 @@ class WoeTree(object):
             return arg
         else:
             return [arg] * shape
+
+    def _fit(self, feature):
+        self._trees[feature].fit(X[:, feature], y)
 
     def fit(self, X, y):
         n_features = X.shape[1]
@@ -47,14 +53,17 @@ class WoeTree(object):
                     smooth_woe=smooth_woe[feature]
                 )
             )
-            self._trees[feature].fit(X[:, feature], y)
+
+        self._trees = (Parallel(n_jobs=self._n_jobs)
+            (delayed(self._trees[feature].fit)(X[:, feature], y)
+                for feature in range(n_features)))
         return self
 
-    def transform(self, X, y):
-        transformed = np.zeros_like(X, dtype=self._dtype)
-        for ind in range(X.shape[1]):
-            transformed[:, ind] = self._trees[ind].transform(X[:, ind])
-        return transformed
+    def transform(self, X):
+        transformed = (Parallel(n_jobs=self._n_jobs)
+            (delayed(self._trees[ind].transform)(X[:, ind])
+                for ind in range(X.shape[1])))
+        return np.array(transformed).T
 
     def fit_transfrom(self, X, y):
         self.fit(X, y)
